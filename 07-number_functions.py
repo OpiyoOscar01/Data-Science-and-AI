8a39769bc8b657fd46fbbbc49a9681765861a1f7
OPIYO OSCAR <148503309+OpiyoOscar01@users.noreply.github.com>
2025-01-09 11:38:56 +0300

    Learnt number functions

---
+++ 07-number_functions.py
@@ -0,0 +1,14 @@
+import math
+pi=3.14159
+print(round(pi))
+print(math.ceil(pi))
+print(math.cos(0))
+print(math.sqrt(16))
+x=-34
+print(abs(x))
+print(math.factorial(5))
+print(math.pow(2,3))
+p,q,r,s,t,u=2,3,4,5,6,7
+print(max(p,q,r,s,t,u))
+print(min(p,q,r,s,t,u))
+print(math.log(100))
