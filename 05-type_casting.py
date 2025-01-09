0fcd384fde3a51154882029fa805cb6a1bca08ae
OPIYO OSCAR <148503309+OpiyoOscar01@users.noreply.github.com>
2025-01-09 10:48:52 +0300

    Learnt type casting

---
+++ 05-type_casting.py
@@ -0,0 +1,9 @@
+name="Mark Steve"
+age=23
+height=5.6
+print("My nama is "+name+" and my age is "+str(age)+" and my height is "+str(height))
+height=int(height)
+
+print("My nama is "+name+" and my age is "+str(age)+" and my height is "+str(height))
+age=float(age)
+print(age)
