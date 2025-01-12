3d82099cb1970e2f47b4617fd6a6d57bec719554
OPIYO OSCAR <148503309+OpiyoOscar01@users.noreply.github.com>
2025-01-12 10:45:23 +0300

    covered return statements in python functions

---
+++ 22-return_values.py
@@ -0,0 +1,7 @@
+# A function can return a value using the return statement. The return statement is used to exit a function and return a value to the caller. The return statement can return a single value or multiple values. The return statement can also return a tuple, list, dictionary, set,an object or any other data type.
+
+def multiply(a, b):
+    return a * b
+
+result = multiply(5, 10)
+print("Result:", result)
