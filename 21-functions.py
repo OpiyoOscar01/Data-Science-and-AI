8b8eab4628471a00e1353cdf2628f4d9b2ad5002
OPIYO OSCAR <148503309+OpiyoOscar01@users.noreply.github.com>
2025-01-12 10:34:30 +0300

    Covered Concepts of functions in Python

---
+++ 21-functions.py
@@ -0,0 +1,17 @@
+# Function is a block of code which only runs when it is called. You can pass data, known as parameters, into a function. A function can return data as a result.
+# To define a function, use the def keyword, followed by the function name and parentheses ().
+# Syntax:
+# def function_name(parameters):
+#     statement(s)
+# function_name: Name of the function.
+# parameters: Input data to the function.
+# statement(s): Block of code to be executed.
+# Example:
+def greetUser(time_of_day):
+  print("Good "+time_of_day+"!")
+
+greetUser("Morning")
+greetUser("Afternoon")
+greetUser("Evening")
+
+# In the above example, greet() is a function that takes a name as a parameter and prints "Hello" followed by the name.
